2ee2cb744f16f070808da99ebfab1296521af0d9
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-07-17 17:25:59 -0600

    Updates

---
+++ precache-manifest.aece183dc020edbd6fa2ed985fc1dc14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beebf6caa2ec86f866df9c18bbcffca7",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "b1465f8d6c207d7fe177",
+    "url": "/project-4-assessment/static/css/main.7e5160f1.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1465f8d6c207d7fe177",
+    "url": "/project-4-assessment/static/js/main.9f09098d.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
